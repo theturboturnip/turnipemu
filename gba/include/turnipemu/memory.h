f87c4d75adb8b332359531c4d64357ced71d2fed
watergun127 <popgoestoast@gmail.com>
2018-07-19 23:16:16 +0100

    Made memory manager return optional<type> when reading from memory

--- gba/include/turnipemu/memory.h
+++ gba/include/turnipemu/memory.h
@@ -4,6 +4,7 @@
 
 #include <assert.h>
 #include <vector>
+#include <optional>
 
 namespace TurnipEmu{
 	class Emulator;
@@ -57,7 +58,7 @@ namespace TurnipEmu{
 		uint32_t endAddress = 0;
 	};
 	
-	class MemoryMap{
+	class MemoryMap {
 		// Read/Write Pseudocode
 		// 1. Check if there's a DMA transfer either TO or FROM this address. Disallow if so. (TODO: Allow writes when transferring from?)
 		// 2. Check if the address is within an AddressableMemoryRange. Disallow if not.
@@ -68,14 +69,17 @@ namespace TurnipEmu{
 		MemoryMap(Emulator& emulator);
 		void registerMemoryController(MemoryController* memoryController);
 
+		void beginTick();
+		void endTick();
+		
 		// Instantiated for byte, halfword, word
 		template<typename ReadType>
-		ReadType read(uint32_t address) const;
+		std::optional<ReadType> read(uint32_t address, bool accessByEmulator = true) const;
 		template<typename WriteType>
-		void write(uint32_t address, WriteType value) const;
+		void write(uint32_t address, WriteType value, bool accessByEmulator = true) const;
 	protected:
-		MemoryController* controllerForAddress(uint32_t address) const; 
-		
+		MemoryController* controllerForAddress(uint32_t address, bool accessByEmulator) const;
+
 		std::vector<MemoryController*> memoryControllers;
 		// TODO: std::unordered_map<uint32_t, MemoryController*> could be used as a cache? Use FIFO to limit total space taken up?
 
@@ -83,7 +87,8 @@ namespace TurnipEmu{
 		
 		// TODO: Remember to make this happen still, just without using a ref here
 		// ARM7TDMI& cpu; // Memory reads take cycles, and affect the CPU registers.
-
+		uint8_t memoryAccessCyclesThisTick;
+		
 		const char* const logTag = "MEM";
 	};
 }
--- gba/src/turnipemu/memorymap.cpp
+++ gba/src/turnipemu/memorymap.cpp
@@ -10,12 +10,58 @@ namespace TurnipEmu{
 		memoryControllers.push_back(memoryController);
 	}
 
-	MemoryController* MemoryMap::controllerForAddress(uint32_t address) const {
+	MemoryController* MemoryMap::controllerForAddress(uint32_t address, bool accessByEmulator) const {
 		for (auto* controller : memoryControllers){
 			if (controller->ownsAddress(address)) return controller;
 		}
-		LogLine(logTag, "Invalid Memory Access at address 0x%08x, stopping", address);
-		emulator.stopped = true;
+		if (accessByEmulator){
+			LogLine(logTag, "Invalid Memory Access at address 0x%08x, stopping", address);
+			emulator.stopped = true;
+		}
 		return nullptr;
 	}
+
+	template<typename ReadType>
+	std::optional<ReadType> MemoryMap::read(uint32_t address, bool accessByEmulator) const {
+		assert(address % sizeof(ReadType) == 0); // Byte reads MUST be on byte-boundaries, halfwords on 2-byte boundaries, words on 4-byte boundaries.
+		
+		auto* controller = controllerForAddress(address, accessByEmulator);
+		if (controller){
+			ReadType value = 0;
+			for (int i = 0; i < sizeof(ReadType); i++){
+				if (controller->allowRead(address + i))
+					value = value | controller->read(address + i) << (8 * i);
+				else{
+					LogLine(logTag, "Memory Read disallowed by controller");
+					emulator.stopped = true;
+				}
+			}
+			return {value};
+		}else{
+			return {};
+		}
+	}
+	template std::optional<byte> MemoryMap::read<byte>(uint32_t, bool) const;
+	template std::optional<halfword> MemoryMap::read<halfword>(uint32_t, bool) const;
+	template std::optional<word> MemoryMap::read<word>(uint32_t, bool) const;
+
+	template<typename WriteType>
+	void MemoryMap::write(uint32_t address, WriteType value, bool accessByEmulator) const {
+		assert(address % sizeof(WriteType) == 0); // Byte writes MUST be on byte-boundaries, halfwords on 2-byte boundaries, words on 4-byte boundaries.
+		
+		auto* controller = controllerForAddress(address, accessByEmulator);
+		if (controller){
+			for (int i = 0; i < sizeof(WriteType); i++){
+				if (controller->allowWrite(address + i))
+					controller->write(address + i, (value >> (8 * i)) & 0xFF);
+				else{
+					LogLine(logTag, "Memory Write disallowed by controller");
+					emulator.stopped = true;
+				}
+			}
+		}
+	}
+	template void MemoryMap::write<byte>(uint32_t, byte, bool) const;
+	template void MemoryMap::write<halfword>(uint32_t, halfword, bool) const;
+	template void MemoryMap::write<word>(uint32_t, word, bool) const;
 }
