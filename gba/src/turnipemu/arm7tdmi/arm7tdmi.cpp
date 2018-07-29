16e9c8c5a3fe24b876cd764a613517d39455e998
watergun127 <popgoestoast@gmail.com>
2018-07-29 17:42:10 +0100

    Added register support for all modes

--- gba/src/turnipemu/arm7tdmi/arm7tdmi.cpp
+++ gba/src/turnipemu/arm7tdmi/arm7tdmi.cpp
@@ -99,25 +99,48 @@ namespace TurnipEmu::ARM7TDMI{
 	
 	const RegisterPointers CPU::registersForCurrentState() {
 		RegisterPointers pointers;
-			
+
+		for (int i = 0; i < 16; i++){
+			pointers.main[i] = &registers.main[i];
+		}
+		pointers.cpsr = &registers.cpsr;
+		
 		switch (registers.cpsr.mode){
 		case Mode::User:
 		case Mode::System:
-			for (int i = 0; i < 16; i++){
-				pointers.main[i] = &registers.main[i];
-			}
-			pointers.cpsr = &registers.cpsr;
 			break;
 		case Mode::Supervisor:
-			for (int i = 0; i < 16; i++){
-				pointers.main[i] = &registers.main[i];
-			}
-			pointers.cpsr = &registers.cpsr;
 			pointers.main[13] = &registers.svc.r13;
 			pointers.main[14] = &registers.svc.r14;
 			pointers.spsr = &registers.svc.spsr;
 			break;
+		case Mode::FIQ:
+			pointers.main[8] = &registers.fiq.r8;
+			pointers.main[9] = &registers.fiq.r9;
+			pointers.main[10] = &registers.fiq.r10;
+			pointers.main[11] = &registers.fiq.r11;
+			pointers.main[12] = &registers.fiq.r12;
+			pointers.main[13] = &registers.fiq.r13;
+			pointers.main[14] = &registers.fiq.r14;
+			pointers.spsr = &registers.fiq.spsr;
+			break;
+		case Mode::IRQ:
+			pointers.main[13] = &registers.irq.r13;
+			pointers.main[14] = &registers.irq.r14;
+			pointers.spsr = &registers.irq.spsr;
+			break;
+		case Mode::Abort:
+			pointers.main[13] = &registers.abt.r13;
+			pointers.main[14] = &registers.abt.r14;
+			pointers.spsr = &registers.abt.spsr;
+			break;
+		case Mode::Undefined:
+			pointers.main[13] = &registers.und.r13;
+			pointers.main[14] = &registers.und.r14;
+			pointers.spsr = &registers.und.spsr;
+			break;
 		default:
+			// This can still happen, if an invalid value is written to the CPSR by the program.
 			throw std::runtime_error(Utils::streamFormat("Illegal Mode for CPSR. Value = ", (int)registers.cpsr.mode, " which is '", ModeString(registers.cpsr.mode), "'"));
 		}
 		
